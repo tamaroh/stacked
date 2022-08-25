f39d2108bb68c24e6d01f6b861ebb0b37af4c735
tamaroh <tamaroh@gmail.com>
2022-08-25 22:02:17 +0900

    installed jest, added one test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import App from './App';
 
 xtest('renders learn react link', () => {
@@ -7,5 +9,12 @@ xtest('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 test("renders data", () => {
-  render()
+  const fakeItem = {
+    name: "Joni Baez",
+  };
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeUser)
+    })
+  );
 })
