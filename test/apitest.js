790697fb6369895da85919b2505d40d2b1c67a9d
tamaroh <tamaroh@gmail.com>
2022-08-26 00:07:48 +0900

    react itemlist

---
+++ test/apitest.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const mongoose = require("mongoose");
+const items = require("../server/items");
+const app = require("../server/index");
+
+describe("API tests", () => {
+    let request;
+    beforeEach(() => {
+        request = chai.request(app);
+    })
+    describe("alldata", () => {
+        it("can display data", () => {
+            request("/", (req, res) => {
+                expect(res[0].name).to.equal("final fantasy 8");
+            })
+        })
+    })
+})
