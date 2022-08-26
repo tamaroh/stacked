9da9cc923393f47dd62156b3c93f580582cd89f8
tamaroh <tamaroh@gmail.com>
2022-08-26 10:23:40 +0900

    set localDB info

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ app.use(express.json());
 app.use(cors());
 
 const port = process.env.DB_PORT || 9000;
-const localDB = "mongodb://127.0.0.1/stacked"
+const localDB = `mongodb://127.0.0.1/${process.env.DB_NAME}`;
 const appCode =
   process.env.MONGODB_URI || 
-//   localDB;
-  process.env.DB_CODE.replace("<password>", process.env.DB_PASS).replace(
-    "/?",
-    `/${process.env.DB_NAME}?`
-  );
+  localDB;
+//   process.env.DB_CODE.replace("<password>", process.env.DB_PASS).replace(
+//     "/?",
+//     `/${process.env.DB_NAME}?`
+//   );
 mongoose.connect(appCode);
 const itemSchema = mongoose.Schema({ name: String });
 const Item = mongoose.model("Item", itemSchema);
--- test/apitest.js
+++ test/apitest.js
@@ -1,14 +1,13 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
-const mongoose = require("mongoose");
-const items = require("../server/items");
 const app = require("../server/index");
 
 describe("API tests", () => {
     let request;
     beforeEach(() => {
         request = chai.request(app);
+        
     })
     describe("alldata", () => {
         it("can display data", () => {
